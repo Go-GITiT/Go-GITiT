530566d1c27cbc0aa8d454b92030f1bb19931264
Demesheo Wilson <demesheochevez@icloud.com>
2015-09-01 14:56:01 -0700

    no fix yet

--- serversidesearch.js
+++ serversidesearch.js
@@ -19,102 +19,11 @@ var libraryCollection = {
 	Aurelia: 0
 };
 
-// var htmlparser = require("htmlparser2");
-// var parser = new htmlparser.Parser({
-//     onopentag: function(name, attribs){
-//         if(name === "script" && attribs.type === "text/javascript"){
-//             console.log("JS! Hooray!");
-//         }
-//     },
-//     ontext: function(text){
-//         console.log("-->", text);
-//     },
-//     onclosetag: function(tagname){
-//         if(tagname === "script"){
-//             console.log("That's it?!");
-//         }
-//     }
-// }, {decodeEntities: true});
-
-var rawHtml = "<!doctype html>\
-<html lang='en'>\
-  <head>\
-    <meta charset='utf-8'>\
-    <meta name='viewport' content='width=device-width, initial-scale=1'>\
-    <title>Relay • Star Wars</title>\
-  </head>\
-  <body>\
-    <div id='root'></div>\
-    <script type='text/javascript'>\
-      // Force `fetch` polyfill to workaround Chrome not displaying request body\
-      // in developer tools for the native `fetch`.\
-      self.fetch = null;\
-      function warnRelayMissing() {\
-        document.body.innerHTML = (\
-          '<h2>Could not find relay.js</h2>' +\
-          '<p>' +\
-            'Be sure to run <code>npm run build</code> ' +\
-            'in the <code>relay/</code> directory.' +\
-          '</p>'\
-        );\
-      }\
-    </script>\
-    <script src='http://localhost:3000/webpack-dev-server.js'></script>\
-    <script src='node_modules/react/dist/react.min.js'></script>\
-    <script src='node_modules/react-relay/dist/relay.js' onerror='warnRelayMissing()'></script>\
-    <script src='js/app.js'></script>\
-  </body>\
-</html>";
-
-var handler = new htmlparser.DomHandler(function (error, dom) {
-    if (error){
-		console.log(error);
-    } else {
-        console.log(dom[1].children[3]);
-    }
-});
-var parser = new htmlparser.Parser(handler);
-parser.write(rawHtml);
-parser.done();
 // regex line breaks, white space, double quotes to singles quotes
 // Save the stringified html content to a variable
 // pull all text between "/*.js"
 // compare libary against list
 // increment appropriate value
-
-
-// parser.write("<!doctype html>\
-// <html lang='en'>\
-//   <head>\
-//     <meta charset='utf-8'>\
-//     <meta name='viewport' content='width=device-width, initial-scale=1'>\
-//     <title>Relay • Star Wars</title>\
-//   </head>\
-//   <body>\
-//     <div id='root'></div>\
-//     <script type='text/javascript'>\
-//       // Force `fetch` polyfill to workaround Chrome not displaying request body\
-//       // in developer tools for the native `fetch`.\
-//       self.fetch = null;\
-//       function warnRelayMissing() {\
-//         document.body.innerHTML = (\
-//           '<h2>Could not find relay.js</h2>' +\
-//           '<p>' +\
-//             'Be sure to run <code>npm run build</code> ' +\
-//             'in the <code>relay/</code> directory.' +\
-//           '</p>'\
-//         );\
-//       }\
-//     </script>\
-//     <script src='http://localhost:3000/webpack-dev-server.js'></script>\
-//     <script src='node_modules/react/dist/react.min.js'></script>\
-//     <script src='node_modules/react-relay/dist/relay.js' onerror='warnRelayMissing()'></script>\
-//     <script src='js/app.js'></script>\
-//   </body>\
-// </html>");
-// parser.end();
-
-
 
 // for html files write a finder function that iterates through all script tags
 	// will find all src files ending in .js, both local, api, and cdnjs hosted
