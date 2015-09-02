433702eddbc111eb4f1ef9e8a7a8f1b3b187b89c
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-02 14:41:28 -0700

    about to change to use big query bot

--- bigQuery.js
+++ bigQuery.js
@@ -1,3 +1,25 @@
 var api = require('./api.js');
+var bigquery = require('bigquery-model');
 
-console.log(api.GOOGLE_KEY);
+bigquery.auth({
+  email: 'mcbrideryan89@gmail.com',
+  key: api.PEM
+});
+
+var table = new bigquery.Table({
+  projectId: 'test1000-1055',
+  datasetId: 'oldstuff',
+  table: 'yes',
+  schema: {
+    fields: [
+      {name: 'repository_owner', type: 'STRING'},
+      {name: 'repository_name', type: 'STRING'},
+      {name: 'repository_created_at', type: 'STRING'}
+    ]
+  }
+});
+
+table.query('SELECT repository_owner, FROM [yes] LIMIT 10;')
+  .then(function(records){
+    console.log(records);
+  });
