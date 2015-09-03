bbd368df7ba342c928e45b7f8b9dd75b2ea764a3
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-03 11:55:41 -0700

    about to break everything

--- bigQuery.js
+++ bigQuery.js
@@ -1,8 +1,9 @@
 /*jshint multistr: true */
 var api = require('./api.js');
 var bigquery = require('bigquery-model');
-
+var unparsed_records;
 var parsed_records = [];
+var async = require('async');
 
 bigquery.auth({
   email: api.EMAIL,
@@ -28,19 +29,32 @@ table.query('SELECT repo.name, repo.url \
   ORDER BY repo.name \
   LIMIT 50')
   .then(function(records){
-    var current = {};
-    records[0].rows.forEach(function(row){
+    unparsed_records = records;
+  })
+  .then(function(){
+    unparsed_records[0].rows.forEach(function(row){   
+      var current = {};
       current.repo_name = row.f[0].v;
       current.repo_url = row.f[1].v;
       parsed_records.push(current);
     });
   })
   .then(function(){
-    console.log(parsed_records[0].repo_name);
-    table.query('SELECT * FROM [gitit.records] WHERE repo_name = "' + parsed_records[0].repo_name + '" LIMIT 10')
-      .then(function(records){
-        console.log('TOTAL ROWS: ', records[0].totalRows);
-      });
+    var count = 0;
+    async.each(parsed_records, function(name){
+      table.query('SELECT * FROM [gitit.records] WHERE repo_name = "' + name.repo_name + '" LIMIT 10')
+        .then(function(records){
+          console.log(++count);
+          if(records[0].tableRows === 0){
+            console.log('THE FOLLOWING WOULD BE INSERTED', name.repo_name);
+          } else {
+            console.log('THE FOLLOWING DATA WOULD NOT BE INSERTED', name.repo_name);
+          }
+        })
+        .catch(function(err){
+          console.error(err);
+        });
+    });
   });
   // iterate over list
   // check if items are in BQ table
