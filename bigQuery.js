735fad49c106f1247543230ee4e9815e761b1897
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-02 20:02:51 -0700

    made some progress

--- bigQuery.js
+++ bigQuery.js
@@ -12,17 +12,32 @@ var table = new bigquery.Table({
   table: 'yes',
 });
 
+var records_table = new bigquery.Table({
+  projectId: 'test1000-1055',
+  datasetId: 'gitit',
+  tableId: 'records'
+})
+
 table.query('SELECT repo.name, repo.url \
   FROM [githubarchive:day.yesterday] \
   WHERE payload CONTAINS \'"language":"JavaScript"\' \
   GROUP EACH BY repo.name, repo.url \
-  ORDER BY repo.name')
+  ORDER BY repo.name \
+  LIMIT 50')
   
   .then(function(records){
+    var parsed_records = [];
+    var current = {};
     records[0].rows.forEach(function(row){
-      row.f.forEach(function(col){
-        console.log(col.v);
-      }); 
+      current.repo_name = row.f[0].v;
+      current.repo_url = row.f[1].v;
+      records_table.push(parsed_records)
+        .then(function(){
+          console.log('FINISHED!');
+        })
+      .catch(function(err){
+        console.error('ERROR ', err);
+      });
     });
   }) 
   .catch(function(err){
