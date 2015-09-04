1dd6ea17c641847e068e16c448a84f3ab2d52458
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-04 11:59:20 -0700

    fixed env variable for mongo db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
   var mongoose = require('mongoose');
 
-  var mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/gitit';
+  var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/gitit';
   mongoose.connect(mongoURI);
 
   var db = mongoose.connection;
