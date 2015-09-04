d80a51e72fc25cc77530980287e28536cfc833c6
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-03 17:06:40 -0700

    bigQuery code cleanup

--- bigQuery.js
+++ bigQuery.js
@@ -1,38 +1,39 @@
 /*jshint multistr: true */
 var api = require('./api.js');
 var bigquery = require('bigquery-model');
-var unparsed_records;
-var parsed_records = [];
-var parsed_legacy = [];
-var final_records = [];
-var queryString;
+var unparsed_records; // VARIABLE TO STORE RAW INCOMING RECORDS
+var parsed_records = []; // ARRAY TO STORE PARSED RECORD INFORMATION
+var parsed_legacy = []; // ARRAY TO STORE PARSED LEGACY DATA FROM GOOGLE BG TABLE
+var final_records = []; // FINAL RECORDS TO BE STORED
+var queryString; // WHERE WE STORE OUR GIANT QUERY STRING
 
-bigquery.auth({
+bigquery.auth({ // AUTHORIZATION INFO FOR GOOGLE BIG QUERY
   email: api.EMAIL,
   key: api.PEM
 });
 
-var table = new bigquery.Table({
+var table = new bigquery.Table({ // TABLE THAT HANDLES GET REQUESTS
   projectId: 'test1000-1055',
   datasetId: 'oldstuff',
   table: 'yes',
 });
 
-var records_table = new bigquery.Table({
+var records_table = new bigquery.Table({ // LEGACY TABLE THAT STORES ALL RECORDS
   projectId: 'test1000-1055',
   datasetId: 'gitit',
   tableId: 'records'
 });
 
+// QUERY TO GET NEW RECORDS FROM YESTERDAY
 table.query('SELECT repo.name, repo.url \
   FROM [githubarchive:day.yesterday] \
   WHERE payload CONTAINS \'"language":"JavaScript"\' \
   GROUP EACH BY repo.name, repo.url \
   ORDER BY repo.name')
-  .then(function(records){
+  .then(function(records){ // STORES RECORDS
     unparsed_records = records;
   })
-  .then(function(){
+  .then(function(){ // PARSES RECORDS
     unparsed_records[0].rows.forEach(function(row){   
       var current = {};
       current.repo_name = row.f[0].v;
@@ -40,17 +41,16 @@ table.query('SELECT repo.name, repo.url \
       parsed_records.push(current);
     });
   })
-  .then(function(){
-    var count = 0;
+  .then(function(){ // CONSTRUCTS GIANT STRING TO CHECK IF NEW RECORDS ALREADY EXIST
     var repo_arr = [];
     parsed_records.forEach(function(name){
       repo_arr.push(name.repo_name);
     });
     queryString = 'SELECT * FROM [gitit.records] WHERE repo_name = "' + repo_arr.join('" OR repo_name = "') + '"';
-  })
-  .then(function(){
+  }) 
+  .then(function(){ // QUERY'S LEGACY TABLE WITH GIANT QUERY STRING
     table.query(queryString)
-      .then(function(records){
+      .then(function(records){ // PARSES INPUT AND STORES IN PARSED LEGACY
         records[0].rows.forEach(function(row){
           var current = {};
           current.repo_name = row.f[0].v;
@@ -58,53 +58,26 @@ table.query('SELECT repo.name, repo.url \
           parsed_legacy.push(current);
         });
       })
-      .then(function(){
+      .then(function(){ // COMPARES STRINGIFIED LEGACY TABLE TO NEW RECORDS
         parsed_legacy = JSON.stringify(parsed_legacy);
-        console.log('TYPE: ', typeof parsed_legacy);
         parsed_records.forEach(function(repo){
           var reg = new RegExp(repo.repo_name, "g");
           if( parsed_legacy.match(reg) === null ){
             final_records.push(repo);
           }
         });
+      })
+      .then(function(){ // CREATES ARRAY OF RECORDS TO BE STORED (THEY ARE NOT DUPLICATES)
+        if(records_table.length > 0){
+          records_table.push(final_records)
+            .then(function(){
+              console.log('FINISHED!');
+            })
+            .catch(function(err){
+              console.error(err);
+            });
+        } else {
+          console.log('NO INSERTIONS!');
+        }
       });
-
   });
-  
-
-
-
-
-     /* table.query('SELECT * FROM [gitit.records] WHERE repo_name = "' + name.repo_name + '" LIMIT 10')
-        .then(function(records){
-          console.log(++count);
-          if(records[0].tableRows === 0){
-            console.log('THE FOLLOWING WOULD BE INSERTED', name.repo_name);
-          } else {
-            console.log('THE FOLLOWING DATA WOULD NOT BE INSERTED', name.repo_name);
-          }
-        })
-        .catch(function(err){
-          console.error(err);
-        });
-    });
-  });
-  // iterate over list
-  // check if items are in BQ table
-  // if they aren't add them
-    // push unique ones into the db
-
-
-    
-/*    records_table.push(parsed_records)
-        .then(function(){
-          console.log('FINISHED!');
-        })
-      .catch(function(err){
-        console.error('ERROR ', err);
-      });
-    });
-  }) 
-  .catch(function(err){
-    console.error('ERROR: ', err);
-});  */
