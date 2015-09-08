dd11b577e40cc30942f7027acf992fca1c1473fc
Doris Hernandez <d.hernz@gmail.com>
2015-09-08 11:08:42 -0700

    testing travis ci 2

--- spec/spec.js
+++ spec/spec.js
@@ -1,12 +1,12 @@
 var expect = require('chai').expect
-,foo = 'bar'
+,foo = 'hello'
 ,beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
 
 describe('testing',function(){
   it('we are testing this thing ', function(){
 
     expect(foo).to.be.a('string');
-    expect(foo).to.equal('bar');
+    expect(foo).to.equal('hello');
     expect(foo).to.have.length(3);
     expect(beverages).to.have.property('tea').with.length(3);
 
