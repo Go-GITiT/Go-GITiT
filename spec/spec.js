a0e265fca7f5ec0396e02544e7db5efbec240bbf
Doris Hernandez <d.hernz@gmail.com>
2015-09-08 10:53:53 -0700

    testing travis 1

--- spec/spec.js
+++ spec/spec.js
@@ -5,17 +5,6 @@ var expect = require('chai').expect
 describe('testing',function(){
   it('we are testing this thing ', function(){
 
-
-    pubnub.subscribe({
-  channel: "gitit_messages",
-  callback: function(message) {
-    console.log("bigQueryWorker > ", message);
-    if (message.type === 'heroku_scheduler_event') {
-     
-    }
-  }
-});
-
     expect(foo).to.be.a('string');
     expect(foo).to.equal('bar');
     expect(foo).to.have.length(3);
