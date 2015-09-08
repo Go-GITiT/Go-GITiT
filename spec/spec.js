0cca7af5af47bc808a1ed73a0e90c9c4d9259de5
Doris Hernandez <d.hernz@gmail.com>
2015-09-08 11:12:25 -0700

    testing travis ci 3

--- spec/spec.js
+++ spec/spec.js
@@ -7,7 +7,7 @@ describe('testing',function(){
 
     expect(foo).to.be.a('string');
     expect(foo).to.equal('hello');
-    expect(foo).to.have.length(3);
+    expect(foo).to.have.length(5);
     expect(beverages).to.have.property('tea').with.length(3);
 
   });
