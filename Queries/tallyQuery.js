34772fec5cb3e3ff08f3fe51f08b0d000c934684
Demesheo Wilson <demesheochevez@icloud.com>
2015-09-08 18:52:52 -0700

    exporting tally

--- Queries/tallyQuery.js
+++ Queries/tallyQuery.js
@@ -1,11 +1,20 @@
 var Tally = require('../Schemas/tally.js').Tally;
 var db = require('../Schemas/config.js');
+
+var currentTally;
+
 Tally.find(function(err,data){
 
-  if(err){
-    throw err;
-  }
+	if(err){
+		throw err;
+	} else {
+		currentTally = JSON.parse(data[data.length-1].tally);
+		
+	}
 
-  // console.log(data);
+
 });
 
+if(currentTally){
+	module.exports.currentTally = currentTally;
+}
