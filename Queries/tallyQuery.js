a729fedcfafae670fa96a4f3a7c1f8ba6c3714b2
Demesheo Wilson <demesheochevez@icloud.com>
2015-09-08 19:36:42 -0700

    Query for tally and callback working

--- Queries/tallyQuery.js
+++ Queries/tallyQuery.js
@@ -1,20 +1,21 @@
 var Tally = require('../Schemas/tally.js').Tally;
 var db = require('../Schemas/config.js');
 
-var currentTally;
+
+
+var currentTally = function(callback){
 
 Tally.find(function(err,data){
+	var lastTally;
 
 	if(err){
 		throw err;
 	} else {
-		currentTally = JSON.parse(data[data.length-1].tally);
-		
+		lastTally = JSON.parse(data[data.length-1].tally);
+		callback(lastTally);
 	}
-
-
 });
+};
 
-if(currentTally){
-	module.exports.currentTally = currentTally;
-}
+module.exports.currentTally = currentTally;
+
