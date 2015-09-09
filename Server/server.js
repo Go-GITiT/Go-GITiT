d657e41438aaa32dbf6eb33b3e27618129f92af3
Demesheo Wilson <demesheochevez@icloud.com>
2015-09-09 11:23:52 -0700

    restructured folders

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ var currentTally = require('./Queries/tallyQuery.js').currentTally;
 var db = require('./Schemas/config.js');
 
 
+app.use(express.static(__dirname + '/Gitit.html'));
+
 var server=app.listen(3000, function(){
 console.log("We have started our server on port 3000");
 db.once('open', function(){
@@ -12,3 +14,4 @@ db.once('open', function(){
 	});
 });
 });
+
