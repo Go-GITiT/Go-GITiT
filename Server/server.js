0019bf684531bd2f852a933c1bb43fbcacc83fa9
Marc Christophe <m@xtof.net>
2015-09-12 16:34:53 -0700

    creating snapshots endpoint

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var currentTally = require('../Queries/tallyQuery.js').currentTally;
+var snapshots = require('../Queries/tallyQuery.js').snapshots;
 var db = require('../Schemas/config.js');
 var path = require('path');
 
@@ -8,22 +9,27 @@ app.use(express.static('../Client'));
 
 var port = process.env.PORT || 1111;
 
-app.listen(port, function(){
-console.log("We have started our server on port " + port);
+app.listen(port, function() {
+  console.log("We have started our server on port " + port);
 
 });
 
-app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, '../Client', 'Gitit.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../Client', 'Gitit.html'));
 });
 
-app.get('/d3DataViz.js', function(req, res){
-	res.sendFile(path.join(__dirname, '../Client', 'd3DataViz.js'));
+app.get('/d3DataViz.js', function(req, res) {
+  res.sendFile(path.join(__dirname, '../Client', 'd3DataViz.js'));
 });
 
-app.get('/tally', function(req, res){
-	var tally;
-	currentTally(function(tally){
-	res.send(JSON.stringify(tally));
-	});
+app.get('/tally', function(req, res) {
+  currentTally(function(tally) {
+    res.send(JSON.stringify(tally));
+  });
+});
+
+app.get('/snapshots', function(req, res){
+  snapshots(function(snaps){
+    res.send(snaps);
+  });
 });
