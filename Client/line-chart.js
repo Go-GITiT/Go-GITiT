556fe15409df4a4260372f553b9dbeffc4b4fe82
Demesheo Wilson <demesheochevez@icloud.com>
2015-09-14 14:57:44 -0700

    Fixed inconsistent variable names

--- Client/line-chart.js
+++ Client/line-chart.js
@@ -65,7 +65,7 @@ function initLineChart() {
 
     for (var fw in frameworkData) {
       vis.append('svg:path')
-        .attr('d', lineGen(frameworks[fw]))
+        .attr('d', lineGen(frameworksData[fw]))
         .attr('stroke', frameworkColor[fw])
         .attr('stroke-width', 2)
         .attr('fill', 'none');
