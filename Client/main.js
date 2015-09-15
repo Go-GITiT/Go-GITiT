2a19b0eaa995b0fb2c702a8bd582cf530c400703
Marc Christophe <m@xtof.net>
2015-09-15 11:03:30 -0700

    creating a clientside main.js

---
+++ Client/main.js
@@ -0,0 +1,39 @@
+var frameworkColor = {
+  react: '#1f77b4',
+  ember: '#ff7f0e',
+  mithril: '#2ca02c',
+  angular: '#d62728',
+  backbone: '#9467bd',
+  polymer: '#e377c2',
+  spine: '#7f7f7f',
+  flight: '#9edae5',
+  knockout: '#771b17',
+  'objective-j': '#d8a93c'
+};
+
+window.onload = function() {
+  initBubbleChart();
+  initBarChart();
+  initLineChart();
+
+  $("#bar-chart").hide();
+  $("#line-chart").hide();
+
+  $("#bubbleChartButton").click(function() {
+    $("#line-chart").hide();
+    $("#bar-chart").hide();
+    $("#bubble-chart").fadeIn();
+  });
+
+  $("#barGraphButton").click(function() {
+    $("#bubble-chart").hide();
+    $("#line-chart").hide();
+    $("#bar-chart").fadeIn();
+  });
+
+  $("#lineGraphButton").click(function() {
+    $("#bubble-chart").hide();
+    $("#bar-chart").hide();
+    $("#line-chart").fadeIn();
+  });
+};
