433cefe47ae1e5e35e943772d1d816b6ef53a1f8
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-16 15:07:52 -0700

    commented out api

--- Schemas/config.js
+++ Schemas/config.js
@@ -1,14 +1,14 @@
-  var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
-  var mongoURI = process.env.MONGOLAB_URI || 'http://localhost:gitit';
+var mongoURI = process.env.MONGOLAB_URI || 'http://localhost:gitit';
 
-  mongoose.connect(mongoURI);
+mongoose.connect(mongoURI);
 
-  var db = mongoose.connection;
-  db.on('error',console.error.bind(console,'connection error'));
-  db.once('open',function(){
-    console.log('Mongodb connection open');
-  });
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error'));
+db.once('open',function(){
+  console.log('Mongodb connection open');
+});
 
-  module.exports = db;
+module.exports = db;
 
