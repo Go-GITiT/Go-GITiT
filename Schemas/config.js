867a098aa8f8ff8ef5abff30c42874db4ca821ff
ryan-mcbride <mcbrideryan89@gmail.com>
2015-09-15 19:10:49 -0700

    thing was published that shouldn't be

--- Schemas/config.js
+++ Schemas/config.js
@@ -1,6 +1,6 @@
   var mongoose = require('mongoose');
 
-  var mongoURI = process.env.MONGOLAB_URI || 'mongodb://heroku_bmkn0cw7:9qnkmthdbkodud914ks46ln1gh@ds035563.mongolab.com:35563/heroku_bmkn0cw7';
+  var mongoURI = process.env.MONGOLAB_URI || 'http://localhost:gitit';
 
   mongoose.connect(mongoURI);
 
